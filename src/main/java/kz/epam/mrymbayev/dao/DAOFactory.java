76410962917cfdbd502e28025a72e582c2c407af
Meirzhan Rymbayev <meirzhan_rumbaev@mail.ru>
2015-12-08 13:06:05 +0600

    Get started to create RdbDAOFactory

--- src/main/java/kz/epam/mrymbayev/dao/DAOFactory.java
+++ src/main/java/kz/epam/mrymbayev/dao/DAOFactory.java
@@ -1,5 +1,24 @@
 package kz.epam.mrymbayev.dao;
 
-public class DAOFactory {
+import org.apache.log4j.Logger;
 
+public abstract class DAOFactory {
+    private static DAOFactory instance;
+    private static Logger logger = Logger.getLogger(DAOFactory.class);
+
+    public static DAOFactory getInstance(){
+        if(instance == null){
+            logger.trace("Creating new JDBC DAO.");
+            instance = new RdbDaoFactory(); // Pattern Data Access Object
+        }
+        return instance;
+    }
+
+    public abstract <T extends GenericDao> T getDao(Class<T> clazz);
+
+    public abstract void beginTransaction();
+    public abstract void commit();
+    public abstract void rollback();
+
+    public abstract void close();
 }
---
+++ src/main/java/kz/epam/mrymbayev/dao/RdbDaoFactory.java
@@ -0,0 +1,5 @@
+package kz.epam.mrymbayev.dao;
+
+public class RdbDaoFactory extends DAOFactory {
+
+}
