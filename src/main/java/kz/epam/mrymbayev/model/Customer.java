0f6e36fdd673db5087ea1bb0d411d2e3ee2391c3
Meirzhan Rymbayev <meirzhan_rumbaev@mail.ru>
2015-12-09 00:09:41 +0600

    toString() for Customer was overrided

--- src/main/java/kz/epam/mrymbayev/model/Customer.java
+++ src/main/java/kz/epam/mrymbayev/model/Customer.java
@@ -19,4 +19,12 @@ public class Customer extends BaseEntity {
     public void setPassword(String password) {
         this.password = password;
     }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "login='" + login + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
 }
